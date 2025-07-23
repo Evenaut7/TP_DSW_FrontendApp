6f7ec0774d335c6cb3bec9569ff3df1fcc91b922
JoaquinMurua <jmurua03@gmail.com>
2025-07-23 09:54:42 -0300

    Inicio de pagina Localidades

--- src/pages/Localidad.tsx
+++ src/pages/Localidad.tsx
@@ -1,6 +1,9 @@
 import Navbar from "../components/Navbar.tsx"
 import { useParams } from "react-router-dom"
 import { useFetchById } from "../reducers/UseFetchByID.ts"
+import "../styles/Localidad.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import imagenlocalidad from "../assets/rosario.webp";
 
 interface Localidad {
   id: number;
@@ -23,19 +26,28 @@ const Localidad = () => {
     localidadId
   );
 
-  if (loading) return <p>Cargando...</p>;
+  if (loading) return <p>Cargand{imagenlocalidad}</p>;
   if (error) return <p>Error: {error}</p>;
   if (!localidad) return <p>No se encontró la localidad</p>;
 
   return (
     <>
       <Navbar/>
+        <div className="topDivLocalidades">
+          <div className="leftTopDivLocalidades">
+            <div className="titleLocalidades">
+              <h3>{localidad.nombre}, Santa Fe</h3>
+              
+            </div>
+            <div className="descriptionLocalidades">
+              <p>Detalles de la localidad</p>
+            </div>
+          </div>
+        </div>
         <div>
-          <h2>Localidad: {localidad.nombre}</h2>
           <p>Cod UTA: {localidad.codUta}</p>
           <p>Latitud: {localidad.latitud}</p>
           <p>Longitud: {localidad.longitud}</p>
-          <p>Provincia ID: {localidad.provincia}</p>
         </div>
     </>    
   );
