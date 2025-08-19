ee71bbd00d32fee74547cf9a7b30f7c7ac12ff53
JoaquinMurua <jmurua03@gmail.com>
2025-08-18 22:58:31 -0300

    Agrega ListadoEventos.Pero esta sin terminar

--- src/pages/PuntoDeInteres.tsx
+++ src/pages/PuntoDeInteres.tsx
@@ -1,9 +1,13 @@
-import ListadoDeTags from '../components/ListadoDeTags.tsx';
 import Navbar from '../components/Navbar';
 import PDI from '../components/PDI';
+import ListadoEventos from '../components/ListadoEventos';
 import { CalendarEvent } from 'react-bootstrap-icons';
+import { useParams } from 'react-router-dom';
 
 const PuntoDeInteres = () => {
+  const { id } = useParams<{ id: string }>();
+  const pdiId = id ? parseInt(id, 10) : null;
+
   return (
     <main className="punto-de-interes">
       <Navbar />
@@ -12,6 +16,8 @@ const PuntoDeInteres = () => {
         <CalendarEvent size={24} />
         Eventos más próximos
       </h3>
+      {pdiId && <ListadoEventos pdiId={pdiId} />}{' '}
+      {/* Forma alternativa para renderizar,diferente a la usada en ListadoPDI. Si quieren la podemos cambiar */}
     </main>
   );
 };
