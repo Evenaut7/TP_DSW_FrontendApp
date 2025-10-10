39ad8c4f2719c66690c1b4fb7343eca558573dfe
Gabitoromero <garomero@frro.utn.edu.ar>
2025-10-10 10:07:23 -0300

    fetch register y login

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -21,7 +21,7 @@ type RegisterModalProps = {
         setError(null);
         setLoading(true);
         try {
-        const res = await fetch('http://localhost:3000/api/usuarios', {
+        const res = await fetch('http://localhost:3000/api/usuarios/register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, gmail, password })
